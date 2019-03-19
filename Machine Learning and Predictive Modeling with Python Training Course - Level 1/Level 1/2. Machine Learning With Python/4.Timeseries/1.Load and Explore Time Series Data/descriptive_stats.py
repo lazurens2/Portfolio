e97c4703ebf54ee21c23222e52c3e698d1b81dbc
lazurens2 <jelidi.mohamad@gmail.com>
2019-03-19 16:44:56 +0100

    Adding the QVF App Customer goods Sales!

--- Machine Learning and Predictive Modeling with Python Training Course - Level 1/Level 1/2. Machine Learning With Python/4.Timeseries/1.Load and Explore Time Series Data/descriptive_stats.py
+++
@@ -1,9 +0,0 @@
-'''
-@author: "Mohamed Jelidi"
-@email : "jelidi.mohamad@gmail.com"
-'''
-
-# calculate descriptive statistics
-from pandas import Series
-series = Series.from_csv('daily-total-female-births.csv', header=0)
-print(series.describe())
--- Machine Learning and Predictive Modeling with Python Training Course - Level 1/Level 1/2. Machine Learning With Python/4.Timeseries/1.Load and Explore Time Series Data/head.py
+++
@@ -1,9 +0,0 @@
-'''
-@author: "Mohamed Jelidi"
-@email : "jelidi.mohamad@gmail.com"
-'''
-
-# summarize first few lines of a file
-from pandas import Series
-series = Series.from_csv('daily-total-female-births.csv', header=0)
-print(series.head(10))
